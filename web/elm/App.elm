baa490888e14867e55f455268ff9d359a9c908cf
Will Strinz <wstrinz@gmail.com>
2016-07-20 18:47:36 -0500

    cloneable

--- web/elm/App.elm
+++ web/elm/App.elm
@@ -1,7 +1,11 @@
 module App exposing (..)
 
-import Html exposing (text)
+import Html exposing (text, div, h1, h2, p)
 
 main : Html.Html a
 main =
-  text "Hello from Elm!"
+  div [] [
+  h1 [] [ text "Welcome to Elm App"]
+  , h2 [] [ text "Make Elm Things"]
+  , p [] [ text "Hooray!"]
+  ]
