9e5bcb2d64df7627f7514ef0779ffb04dc4b9bd7
Will Strinz <wstrinz@gmail.com>
2016-08-02 14:35:10 -0500

    wip http test

--- web/elm/App.elm
+++ web/elm/App.elm
@@ -1,11 +1,13 @@
 module App exposing (..)
 
-import Html exposing (text, div, h1, h2, p)
+import Html exposing (text, div, h1, h2, p, br, button)
 import Html.App
+import Html.Events exposing (onClick)
 import Mouse
+import Task
+import Http
+import Json.Decode as Json
 import Geolocation exposing (Location, changes)
-
-
 
 type alias Loc =
   { latitude : Float
@@ -14,17 +16,22 @@ type alias Loc =
 
 type alias Model =
   { location: Loc,
+    cmdstatus: String,
     boards: Int }
+
 type Msg
     = FooMsg Int Int
     | LocMsg Location
+    | MakeHttpReq
+    | HttpRespSuccess
+    | HttpRespFail
 
 init : ( Model, Cmd Msg )
 init =
-    ( { location = {
-          latitude = 0.0, longitude = 0.0, timestamp = 0
-        },
-        boards = 0 }, Cmd.none )
+    ( { location = { latitude = 0.0, longitude = 0.0, timestamp = 0 }
+        , boards = 0
+        , cmdstatus = "none" }
+      , Cmd.none )
 
 
 main : Program Never
@@ -36,11 +43,13 @@ main =
         , subscriptions = subscriptions
         }
 
-view : Model -> Html.Html a
+view : Model -> Html.Html Msg
 view model =
   div [] [
   h1 [] [ text "Welcome to Night Elm App"]
   , p [] [ text (toString model) ]
+  , br [] []
+  , button [ onClick MakeHttpReq ] [ text "Make Request" ]
   ]
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -58,6 +67,24 @@ update msg model =
           timestamp = l.timestamp }
       in
         ({model | location = newloc }, Cmd.none)
+
+    MakeHttpReq ->
+      (model, fetchNearby)
+    HttpRespSuccess ->
+      ({model | cmdstatus = "resp success"}, Cmd.none)
+    HttpRespFail ->
+      ({model | cmdstatus = "resp fail"}, Cmd.none)
+
+fetchNearby : Cmd Msg
+fetchNearby =
+  let
+    url = "/api/test"
+  in
+    Task.perform (\_ -> HttpRespFail) (\_ -> HttpRespSuccess) (Http.get decodeApiResp url)
+
+decodeApiResp : Json.Decoder String
+decodeApiResp =
+  Json.at ["data"] Json.string
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
